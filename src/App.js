a5e21e1f135e0f150a4cefb8eee5ea37043d6575
freelancercoder9 <85793659+freelancercoder9@users.noreply.github.com>
2023-03-05 13:02:41 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <h1 className="text-3xl font-bold text-red-600 underline">Hello world!</h1>;
+}
+
+export default App;
